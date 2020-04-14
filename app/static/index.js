e37d085c34024f70e8a65b13a47f0d27d4000bba
Keely Wan <keelywan@Keelys-MBP.fios-router.home>
2020-04-14 15:53:31 -0400

    get input links in front end

--- app/static/index.js
+++ app/static/index.js
@@ -11,7 +11,7 @@ function addInput() {
   // https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
   // source used to add/remove elements
   var new_input = document.getElementById("input_text").value;
-  if (inputSet.has(new_input) == false && new_input != "" && new_input != "Enter input.") {
+  if (inputSet.has(new_input) == false && new_input != "" && new_input != "Enter input." && new_input.includes("https://open.spotify.com/playlist/")) {
     inputSet.add(new_input);
     const div = document.createElement('div');
     div.className = 'row';
@@ -31,7 +31,16 @@ function removeRow(input) {
 }
 
 function output() {
-  var data = "link=https://open.spotify.com/playlist/5hOxxrUnRYpf6XVScyjF0Y&link=https://open.spotify.com/playlist/48KXkzzA9xkonptFgWx1a9"
+  var data = "";
+  inputSet.forEach((item) => {
+    if (data === "") {
+      data += ("link=" + item);
+    }
+    else {
+      data += ("&link=" + item);
+    }
+  })
+  // var data = "link=https://open.spotify.com/playlist/5hOxxrUnRYpf6XVScyjF0Y&link=https://open.spotify.com/playlist/48KXkzzA9xkonptFgWx1a9"
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "http://0.0.0.0:5000", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
