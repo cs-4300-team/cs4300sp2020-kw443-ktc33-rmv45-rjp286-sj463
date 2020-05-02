ac0f20597596cb4c7b315591cbd8a084cfb5b0cd
rpappa <rjpappa@gmail.com>
2020-05-02 16:04:23 -0400

    svd results to database

--- app/irsystem/algorithm/svd_text_mining.py
+++ app/irsystem/algorithm/svd_text_mining.py
@@ -11,6 +11,7 @@ from scipy.sparse.linalg import svds
 from app.database import database
 from app.spotify import spotify
 from collections import defaultdict
+import time
 
 db_songs = {}
 
@@ -26,26 +27,29 @@ def get_comments():
             reddit_comments += comment["text"]
         reddit_obj.append({"song_id": song_id, "comment": reddit_comments})
     """
-    for i in range(0, 1000): 
-        song_id = songs[i]["id"]
-        if song_id not in db_songs:
-            db_songs[song_id] = {
-                "name": songs[i]["name"],
-                "artists": list(map(lambda a: a["name"], songs[i]["artists"])),
-                "images": songs[i]["album"]["images"],
-                "id": songs[i]["id"]
-            }
-        reddit_comments = " ".join(list(map(lambda c: c["text"], songs[i]["comments"])))
+    done = 0
+    idx_to_id = {}
+    for song in songs: 
+        song_id = song["id"]
+        db_songs[song_id] = {
+            "name": song["name"],
+            "artists": list(map(lambda a: a["name"], song["artists"])),
+            "images": song["album"]["images"],
+            "id": song["id"]
+        }
+        reddit_comments = " ".join(list(map(lambda c: c["text"], song["comments"])))
         # for comment in songs[i]["comments"]:
         #     reddit_comments += comment["text"]
         reddit_obj.append({"song_id": song_id, "comment": reddit_comments})
-        print(i)
-    return reddit_obj
+        idx_to_id[done] = song_id
+        done += 1
+        print(done)
+    return (reddit_obj, idx_to_id)
 
 
 def svd():
     print("running svd")
-    redditcomments = get_comments()
+    redditcomments, idx_to_id = get_comments()
     print("got comments")
     documents = [(x['song_id'], x['comment']) for x in redditcomments]
     vectorizer = TfidfVectorizer(stop_words = 'english', max_df = .7, min_df = 1)
@@ -90,14 +94,22 @@ def svd():
         return [(documents[i][0],sims[i]/sims[asort[0]]) for i in asort[1:]]
     
     scores = defaultdict(int)
-    for i in range(0, 1000):
-        # song = documents[i][0]
-        for title, score in closest_projects(i):
-            scores[title] += score
-            # print("{}:{:.3f}".format(title[:40], score))
-    top = sorted(scores.keys(), key=lambda k: scores[k], reverse=True)[0:10]
-    top_named = list(map(lambda k: db_songs[k], top))
-    print(top_named)
+    for i in idx_to_id:
+        similar = []
+        for title, score in closest_projects(i, 30):
+            similar.append({"id": title, "score": score})
+        database.put_sim_songs(idx_to_id[i], similar)
+        print(i)
+
+    # for i in range(0, 1000):
+    #     # song = documents[i][0]
+    #     for title, score in closest_projects(i):
+    #         scores[title] += score
+    #         # print("{}:{:.3f}".format(title[:40], score))
+    # top = sorted(scores.keys(), key=lambda k: scores[k], reverse=True)[0:10]
+    # top_named = list(map(lambda k: db_songs[k], top))
+    # print(top_named)
+    return "done"
 
 
     """
